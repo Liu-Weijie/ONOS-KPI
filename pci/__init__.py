57450b854ab953e0eee405f9eb3c3ddf8fd37982
LiuWeijie <weijie.liu@intel.com>
2022-07-13 16:29:49 +0800

    add ran parameter in xapp

--- pci/__init__.py
+++ pci/__init__.py
@@ -128,22 +128,16 @@ async def subscribe(e2_client: E2Client, e2_node_id: str, trigger_type: RcPreTri
         )
         logging.info(f"control head : {ControlHeader}")
         # control message
-        parameter_type = []
-        parameter_val = []
-        parameter_type.append(
-            RanparameterDefItem(
-                ran_parameter_id=RanparameterId(value=1),
-                ran_parameter_name=RanparameterName(value="result"),
-                ran_parameter_type=RanparameterType.RANPARAMETER_TYPE_INTEGER,
-        ))
-        parameter_val.append(
-            RanparameterValue(
-                value_int=1
-        ))
         ControlMessage = E2SmRcPreControlMessage(
             control_message=E2SmRcPreControlMessageFormat1(
-                parameter_type=parameter_type,
-                parameter_val=parameter_val,
+                parameter_type=RanparameterDefItem(
+                    ran_parameter_id=RanparameterId(value=1),
+                    ran_parameter_name=RanparameterType.RANPARAMETER_TYPE_INTEGER,
+                    ran_parameter_type=RanparameterName(value="Result")
+                ),
+                parameter_val=RanparameterValue(
+                    value_int=1
+                ),
             )
         )
         logging.info(f"control message : {ControlMessage}")
